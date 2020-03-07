dc24ca3e8144c5c7065b6e1bd202fac9582b3793
DaniilShcherbakov <Charel@bk.ru>
2020-03-07 18:36:15 +0300

    Create project

---
+++ app/src/main/java/ru/nvg_soft/basketballstat/MainActivity.kt
@@ -0,0 +1,37 @@
+package ru.nvg_soft.basketballstat
+
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var textMessage: TextView
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                textMessage.setText(R.string.title_home)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_reports -> {
+                textMessage.setText(R.string.title_reports)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_settings -> {
+                textMessage.setText(R.string.title_settings)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        textMessage = findViewById(R.id.message)
+        navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
+}
