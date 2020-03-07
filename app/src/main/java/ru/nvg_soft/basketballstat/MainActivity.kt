ea59b3aa3fa6f62b5d5c67ca63efa77f9ee97fd8
DaniilShcherbakov <Charel@bk.ru>
2020-03-07 21:26:58 +0300

    Create fragments from BottomNavigation

--- app/src/main/java/ru/nvg_soft/basketballstat/MainActivity.kt
+++ app/src/main/java/ru/nvg_soft/basketballstat/MainActivity.kt
@@ -2,24 +2,29 @@ package ru.nvg_soft.basketballstat
 
 import android.os.Bundle
 import android.support.design.widget.BottomNavigationView
+import android.support.v4.app.Fragment
 import android.support.v7.app.AppCompatActivity
 import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var textMessage: TextView
+
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.navigation_home -> {
-                textMessage.setText(R.string.title_home)
+                val fragment = HomeFragment()
+                addFragment(fragment)
                 return@OnNavigationItemSelectedListener true
+
             }
             R.id.navigation_reports -> {
-                textMessage.setText(R.string.title_reports)
+                val fragment = ReportFragment()
+                addFragment(fragment)
                 return@OnNavigationItemSelectedListener true
             }
             R.id.navigation_settings -> {
-                textMessage.setText(R.string.title_settings)
+                val fragment = SettingsFragment()
+                addFragment(fragment)
                 return@OnNavigationItemSelectedListener true
             }
         }
@@ -31,7 +36,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
-        textMessage = findViewById(R.id.message)
+
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+        val fragment = HomeFragment()
+        addFragment(fragment)
+    }
+   private fun addFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.container, fragment, fragment.javaClass.getSimpleName())
+            .commit()
     }
 }
