ce5dab75ea407c56ee2b4dd33b394e9aa88aa6f0
DaniilShcherbakov <Charel@bk.ru>
2020-05-09 14:24:58 +0300

    create database room

---
+++ app/src/main/java/ru/nvg_soft/basketballstat/models/database/MyDatabase.kt
@@ -0,0 +1,37 @@
+package ru.nvg_soft.basketballstat.models.database
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import ru.nvg_soft.basketballstat.models.Event
+import ru.nvg_soft.basketballstat.models.Person
+
+
+@Database(entities = [Person::class, Event::class], version = 1)
+abstract class MyDatabase:RoomDatabase() {
+    abstract fun personDao():PersonDao
+    abstract fun eventDao():EventDao
+    companion object {
+        @Volatile
+        private var instance: MyDatabase? = null
+
+        fun getInstance(context: Context): MyDatabase? {
+            if (instance == null) {
+                synchronized(MyDatabase::class) {
+                    instance = Room.databaseBuilder(
+                        context.applicationContext,
+                        MyDatabase::class.java, "coach_note"
+                    )
+                        .fallbackToDestructiveMigration()
+                        .build()
+                }
+            }
+            return instance
+        }
+    }
+
+    fun destroyInstance(){
+        instance = null
+    }
+}
